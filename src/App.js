42907a16b3fccd3b6e008f02d1d9a0fd1f12dd6f
Rahat <djrayhan8@gmail.com>
2021-03-02 00:02:06 +0600

    added router and made the first route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,30 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import useFetch from "../../Hooks/useFetch";
 import BlogList from "../Blog/BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: "My new website",
-            body: "lorem ipsum...",
-            author: "mario",
-            id: 1,
-        },
-        {
-            title: "Welcome party!",
-            body: "lorem ipsum...",
-            author: "yoshi",
-            id: 2,
-        },
-        {
-            title: "Web dev top tips",
-            body: "lorem ipsum...",
-            author: "mario",
-            id: 3,
-        },
-    ]);
+    const url = "http://localhost:8000/blogs";
+    const { data: blogs, isPending, error } = useFetch(url);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} />
+            {error && <div>{error} </div>}
+            {isPending && <div> Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 };
